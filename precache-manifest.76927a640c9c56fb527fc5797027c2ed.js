67fec505155bd8e3e9e47120f1aa8241a509f4c7
Airas Yaqub <airasyaqub6@gmail.com>
2018-11-12 12:32:19 +0500

    Updates

---
+++ precache-manifest.76927a640c9c56fb527fc5797027c2ed.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ac9c01ea777dc2f37fcc01b176ca2c2",
+    "url": "/react_fb_post_generator/static/media/hahaed.8ac9c01e.PNG"
+  },
+  {
+    "revision": "d40a2295a16becb6befc",
+    "url": "/react_fb_post_generator/static/css/main.e298faa4.chunk.css"
+  },
+  {
+    "revision": "671c48f91c090eac1f6d359d66a1d5c8",
+    "url": "/react_fb_post_generator/static/media/privacy-friends.671c48f9.PNG"
+  },
+  {
+    "revision": "6b3eec9dfcb0f964119b",
+    "url": "/react_fb_post_generator/static/js/1.6b3eec9d.chunk.js"
+  },
+  {
+    "revision": "45bcfb2e3affc56b04c8",
+    "url": "/react_fb_post_generator/static/js/runtime~main.45bcfb2e.js"
+  },
+  {
+    "revision": "a52fa73fc13846f0e54ee22547b35974",
+    "url": "/react_fb_post_generator/static/media/liked.a52fa73f.PNG"
+  },
+  {
+    "revision": "bdbc3dfc663c117c02d716f10c711bf3",
+    "url": "/react_fb_post_generator/static/media/loved.bdbc3dfc.PNG"
+  },
+  {
+    "revision": "d40a2295a16becb6befc",
+    "url": "/react_fb_post_generator/static/js/main.d40a2295.chunk.js"
+  },
+  {
+    "revision": "71de4c77e40980f25cf4640336fb1885",
+    "url": "/react_fb_post_generator/static/media/wowed.71de4c77.PNG"
+  },
+  {
+    "revision": "cf4195a9c7a1c1712b209dbb514dd450",
+    "url": "/react_fb_post_generator/static/media/saded.cf4195a9.PNG"
+  },
+  {
+    "revision": "0b7a139e282a4d9cfc13238921ad3c70",
+    "url": "/react_fb_post_generator/static/media/angred.0b7a139e.PNG"
+  },
+  {
+    "revision": "54778f625486ef4832da050a0b4f5d76",
+    "url": "/react_fb_post_generator/static/media/privacy-public.54778f62.PNG"
+  },
+  {
+    "revision": "6b3eec9dfcb0f964119b",
+    "url": "/react_fb_post_generator/static/css/1.f74a2967.chunk.css"
+  },
+  {
+    "revision": "87b80909c9c72a436d64f33f11a2a02b",
+    "url": "/react_fb_post_generator/index.html"
+  }
+];
